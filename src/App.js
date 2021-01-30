140b569d1e0efb75d30dbf1d8adb03079e9569ba
Keerat_Grewal <kgrewa03@calpoly.edu>
2021-01-30 15:39:53 -0800

    Added router stuff

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
 import Search from './components/Search'
 import Navigation from './components/Navigation'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from './components/Login'
+import Home from './components/Home'
+// import Create from './components/Create'
 
 function App() {
   return (
-    <div className="App">
-      <Navigation></Navigation>
-      <Search></Search>
-    </div>
+      <BrowserRouter>
+        <div className="App">
+          <Navigation />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/login" component={Login} />
+            <Route path="/search" component={Search} />
+            {/* <Route path="/create" component={Create} /> */}
+          </Switch>
+        </div>
+      </BrowserRouter>
   );
 }
 
