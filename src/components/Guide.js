f9fda2550ec638505f577dad3b2392870544c463
Keerat_Grewal <kgrewa03@calpoly.edu>
2021-01-30 11:52:06 -0800

    Added Guide

--- src/components/Guide.js
+++ src/components/Guide.js
@@ -1,30 +1,45 @@
 import React, {Component} from 'react';
-import { Row } from 'react-bootstrap';
-import { Col } from 'react-bootstrap';
-import {Dropdown} from 'react-bootstrap';
-
-// import {connect} from 'react-redux';
-// import {login} from '../../store/actions/authActions';
-// import {Redirect, Link} from 'react-router-dom';
+import { Row, Col, Container} from 'react-bootstrap';
+import {Dropdown, ButtonGroup, DropdownButton} from 'react-bootstrap';
+import {Link, NavLink} from 'react-router-dom'
+import "../styles/guide.css";
+import { linkSync } from 'fs';
 
 class Guide extends Component{
     constructor(props){
         super(props);
+        this.state={links: ["https://react-bootstrap.github.io/getting-started/introduction",
+        "https://react-bootstrap.github.io/getting-started/introduction", "https://react-bootstrap.github.io/getting-started/introduction",
+        "https://react-bootstrap.github.io/getting-started/introduction"], title:""};
+    }
+
+    componentDidMount(){
+      // make axios call here to get the title/links
+      this.setState({title:"React Guide"})
     }
 
     render(){
         return(
-          <Dropdown>
-            <Dropdown.Toggle variant="success" id="dropdown-basic">
-              Dropdown Button
-            </Dropdown.Toggle>
-          
-            <Dropdown.Menu>
-              <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-              <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-              <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
+          <Container>
+            <DropdownButton
+            title={this.state.title}
+            id="guideStyle"
+            >
+              {this.state.links &&
+                this.state.links.map((link, index) => {
+                  return (
+                    <Dropdown.Item
+                      id = "linkStyle"
+                      href = {link}
+                      target = "_blank" 
+                      rel = "noopener noreferrer"
+                    >
+                      Link {index+1} : Click here
+                    </Dropdown.Item>
+                    );
+                })}
+            </DropdownButton>
+          </Container>
         )
     }
 }
